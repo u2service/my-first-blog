c5d56a18648a7d4e66c198bf49f299ee918fc09f
u2service <u2service60@gmail.com>
2025-06-07 10:19:21 +0900

    投稿を降順（新しいものが上）で表示２

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # published_date フィールドを降順(新しい順)にソート
+    posts = Post.objests.all().order_by('-published_date')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
